dd462ce055514441277e48b17dcdae7c684d5130
Ngina-G <Nginagitau11@outlook.com>
2022-03-06 21:15:09 +0300

    updated code for the events

--- src/index.js
+++ src/index.js
@@ -16,6 +16,28 @@ let maleAkanName={
     Friday: "Afua",
     Saturday: "Ama"
 }
+
+    //FINDS DATE INPUT
+
+let days=['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+
+function dateResults() {
+    let dateInput = document.getElementById('date');
+        let dayOfWeek = new Date(dateInput.value).getDay();
+        let birthday= days[dayOfWeek];
+        console.log(birthday);
+           return (birthday);  
+           
+    }
+const birthday= dateResults()
+console.log(birthday + "yip")
+// let res= ()=> {
+//     const input = birthday;
+//     console.log(input);
+// }
+// alert(res())
+
    //FINDS MALE ARRAY
 
 const foundDayArrayMale= Object.getOwnPropertyNames(maleAkanName)
@@ -26,47 +48,32 @@ const foundDayArrayMale= Object.getOwnPropertyNames(maleAkanName)
 const foundDayArrayfemale= Object.getOwnPropertyNames(femaleAkanName)
     console.log(foundDayArrayfemale);
     
-    //FINDS DATE INPUT
-
-let days=['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
-function dateResults() {
-    let dateInput = document.getElementById('date');
-        let dayOfWeek = new Date(dateInput.value).getDay();
-        let birthday= days[dayOfWeek];
-        console.log(birthday);
-        return (birthday);
-    }
+let comparisonMale= foundDayArrayMale.find(element => element === dateResults());
+console.log(comparisonMale)
+let comparisonFemale= foundDayArrayfemale.find(element => element === dateResults());
 
     //FUNCTION TO RETURN AKAN NAME 
 
-function genderFunc() {
-    let comparisonMale= foundDayArrayMale.find(element => element === dateResults());
-    console.log(comparisonMale);
-    let comparisonFemale= foundDayArrayfemale.find(element => element === dateResults());
-    console.log(comparisonFemale);
-    let genderInput = document.getElementById("gender").value;
-        if(genderInput == "male"){
+let genderFunc = () =>{
+        let genderInput = document.getElementById("gender").value;
+        if(genderInput == document.getElementById("male").value){
             return (maleAkanName[comparisonMale])
-        }else if(genderInput == "female"){
+        }else if(genderInput == document.getElementById("female").value){
             return (femaleAkanName[comparisonFemale])
-        }
-}
+        }}
+        console.log(genderFunc())
+    
 
  //RETURNING RESULTS FROM THE FORM
 
 let formResults= () =>{
-    
+    let popup = document.getElementById("popup");
+    popup.classList.toggle("show");
+    document.getElementById("popup").innerHTML = 
+        "Great! " + document.getElementById("name").value + ", Your Akan name is, " + genderFunc()
+        + " Which means you were born on a " + dateResults();
 }
-        
-
-function final(gender){
-    if(gender == "male"){
-        document.write
-    }
-}
-
-console.log(final('female'))
-
+function myFunction() {
    
+}         
 
